fac85502e71230148bcae7f844606a2d9ae966b4
LukaLuka5 <lukaserbia@hotmail.com>
2021-10-06 22:34:43 +0200

    commit spring boot initializer project

---
+++ movieRestApp/src/test/java/com/luka/movieRestApp/MovieRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luka.movieRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
